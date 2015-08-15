a0b16d49ef9cc0fe796902511821f40f5e9afa08
zhboner <zhboner@outlook.com>
2015-08-15 16:04:38 +1000

    add css

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var crypto = require('crypto'),
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        Post.get(null, function (err, posts) {
+        Post.getAll(null, function (err, posts) {
             if (err) {
                 posts = [];
             }
@@ -135,6 +135,62 @@ module.exports = function (app) {
         req.session.user = null;
         req.flash('success', '登出成功!');
         res.redirect('/');//登出成功后跳转到主页
+    });
+
+    app.get('/upload', checkLogin);
+    app.get('/upload', function (req, res) {
+        res.render('upload', {
+            title: '文件上传',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+
+    app.post('/upload', checkLogin);
+    app.post('/upload', function (req, res) {
+        req.flash('success', '文件上传成功!');
+        res.redirect('/upload');
+    });
+
+    app.get('/u/:name', function (req, res) {
+        //检查用户是否存在
+        User.get(req.params.name, function (err, user) {
+            if (!user) {
+                req.flash('error', '用户不存在!');
+                return res.redirect('/');//用户不存在则跳转到主页
+            }
+            //查询并返回该用户的所有文章
+            Post.getAll(user.name, function (err, posts) {
+                if (err) {
+                    req.flash('error', err);
+                    return res.redirect('/');
+                }
+                res.render('user', {
+                    title: user.name,
+                    posts: posts,
+                    user : req.session.user,
+                    success : req.flash('success').toString(),
+                    error : req.flash('error').toString()
+                });
+            });
+        });
+    });
+
+    app.get('/u/:name/:day/:title', function (req, res) {
+        Post.getOne(req.params.name, req.params.day, req.params.title, function (err, post) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('article', {
+                title: req.params.title,
+                post: post,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
     });
 
 };
